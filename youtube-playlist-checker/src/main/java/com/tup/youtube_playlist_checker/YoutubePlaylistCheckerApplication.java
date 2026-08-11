2f4d78705283d28fa2214d9dc7247d40b169c497
Luca-Escudero <luca.escudero2.0@gmail.com>
2026-08-11 16:12:49 -0300

    Add initial project structure with Maven configuration and application files

---
+++ youtube-playlist-checker/src/main/java/com/tup/youtube_playlist_checker/YoutubePlaylistCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.tup.youtube_playlist_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubePlaylistCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubePlaylistCheckerApplication.class, args);
+	}
+
+}
---
+++ youtube-playlist-checker/src/test/java/com/tup/youtube_playlist_checker/YoutubePlaylistCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tup.youtube_playlist_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubePlaylistCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
