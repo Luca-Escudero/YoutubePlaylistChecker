ba34ee89fb0f07feb6d2a3046893f0d85ff9a26f
TomiGrasso <tomiigrasso@gmail.com>
2026-08-24 16:06:47 -0300

    Implementación y prueba de CRUD de Playlist

---
+++ youtube-playlist-checker/src/main/java/com/tup/youtube_playlist_checker/controllers/PlaylistController.java
@@ -0,0 +1,59 @@
+package com.tup.youtube_playlist_checker.controllers;
+
+import com.tup.youtube_playlist_checker.entity.Playlist;
+import com.tup.youtube_playlist_checker.services.PlaylistService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/playlists")
+public class PlaylistController {
+
+    private final PlaylistService playlistService;
+
+    public PlaylistController(PlaylistService playlistService) {
+        this.playlistService = playlistService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Playlist>> obtenerTodas() {
+        return ResponseEntity.ok(playlistService.obtenerTodas());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Playlist> obtenerPorId(@PathVariable Long id) {
+        Optional<Playlist> playlist = playlistService.obtenerPorId(id);
+
+        if (playlist.isPresent()) {
+            return ResponseEntity.ok(playlist.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/youtube/{youtubeId}")
+    public ResponseEntity<Playlist> obtenerPorYoutubeId(@PathVariable String youtubeId) {
+        Optional<Playlist> playlist = playlistService.obtenerPorYoutubeId(youtubeId);
+
+        if (playlist.isPresent()) {
+            return ResponseEntity.ok(playlist.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Playlist> guardar(@RequestBody Playlist playlist) {
+        Playlist playlistGuardada = playlistService.guardar(playlist);
+        return ResponseEntity.ok(playlistGuardada);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+        playlistService.eliminar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ youtube-playlist-checker/src/main/java/com/tup/youtube_playlist_checker/repositories/ConsultaRepository.java
@@ -0,0 +1,13 @@
+package com.tup.youtube_playlist_checker.repositories;
+
+import com.tup.youtube_playlist_checker.entity.Consulta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ConsultaRepository extends JpaRepository <Consulta, Long>{
+
+    Optional<Consulta> findById(Long id);
+}
