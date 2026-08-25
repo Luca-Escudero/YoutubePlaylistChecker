8cb067f0054c39c4135bd3f989d054e1b2ef04d7
TomiGrasso <tomiigrasso@gmail.com>
2026-08-25 00:27:29 -0300

    Integración de la API de YT y prueba de endpoints

--- youtube-playlist-checker/src/main/java/com/tup/youtube_playlist_checker/services/PlaylistService.java
+++ youtube-playlist-checker/src/main/java/com/tup/youtube_playlist_checker/services/PlaylistService.java
@@ -4,6 +4,7 @@ import com.tup.youtube_playlist_checker.entity.Playlist;
 import com.tup.youtube_playlist_checker.repositories.PlaylistRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,9 +12,11 @@ import java.util.Optional;
 public class PlaylistService {
 
     private final PlaylistRepository playlistRepository;
+    private final YoutubeService youtubeService;
 
-    public PlaylistService(PlaylistRepository playlistRepository) {
+    public PlaylistService(PlaylistRepository playlistRepository, YoutubeService youtubeService) {
         this.playlistRepository = playlistRepository;
+        this.youtubeService = youtubeService;
     }
 
     public List<Playlist> obtenerTodas() {
@@ -26,6 +29,39 @@ public class PlaylistService {
 
     public Optional<Playlist> obtenerPorYoutubeId(String youtubeId) {
         return playlistRepository.findByYoutubeId(youtubeId);
+    }
+
+    public Playlist obtenerOCrearDesdeYoutube(String youtubeId) {
+
+        // Buscamos en BD
+        Optional<Playlist> playlistExistente = playlistRepository.findByYoutubeId(youtubeId);
+
+        if (playlistExistente.isPresent()) {
+            return playlistExistente.get();
+        }
+
+        // Si no existe, consultamos YouTube
+        YoutubeService.YoutubePlaylist playlistYoutube = youtubeService.obtenerPlaylist(youtubeId);
+
+        if (playlistYoutube == null) {
+            return null;
+        }
+
+        // Creamos nuestra entidad
+        Playlist playlist = new Playlist();
+
+        playlist.setYoutubeId(playlistYoutube.youtubeId());
+        playlist.setTitulo(playlistYoutube.titulo());
+        playlist.setCantidadVideos(playlistYoutube.cantidadVideos());
+
+        playlist.setUrl(
+                "https://www.youtube.com/playlist?list=" + youtubeId
+        );
+
+        playlist.setFechaPrimeraConsulta(LocalDateTime.now());
+
+        // Guardamos en BD
+        return playlistRepository.save(playlist);
     }
 
     public Playlist guardar(Playlist playlist) {
